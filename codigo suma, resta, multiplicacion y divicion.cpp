20f687040a33a99ce8cce2c7bc85eb166a8d072b
Eugenio-Cruz <eugeniocruz683@gmail.com>
2026-03-25 18:37:30 -0600

    codigo suma, resta, multiplicacion y divicion en codigo c++
    codigo suma, resta, multiplicacion y divicion en codigo c++

---
+++ codigo suma, resta, multiplicacion y divicion.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+programa: Suma, Resta, Multiplicacion y Division
+AlUMNO: Eugenio Alejandro Cruz Hernandez
+fecha: 25/03/2026
+*******************************************************************************/
+
+#include <iostream>
+
+int main() {
+    float numero1, numero2;
+
+    std::cout << "Escribe el primer número: ";
+    std::cin >> numero1;
+
+    std::cout << "Escribe el segundo número: ";
+    std::cin >> numero2;
+
+    float suma = numero1 + numero2;
+    float resta = numero1 - numero2;
+    float multiplicacion = numero1 * numero2;
+    float division = numero1 / numero2;
+
+    std::cout << "La suma es: " << suma << std::endl;
+    std::cout << "La resta es: " << resta << std::endl;
+    std::cout << "La multiplicación es: " << multiplicacion << std::endl;
+    std::cout << "La division es: " << division << std::endl;
+
+    return 0;
+}
